e8e398d98b4d53403b4a1cefceb6ad2791c48f4d
Bersbot <biosyrok0@gmail.com>
2024-10-23 19:41:27 +0200

    g

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -52,11 +52,6 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/bersbot/proj/Cpp_/build/include/glad/cmake_install.cmake")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/bersbot/proj/Cpp_/build/include/glew/build/cmake/cmake_install.cmake")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
