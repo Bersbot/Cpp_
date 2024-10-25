0014b0f3ac7fda889ea861a0267c305aadd816a7
Bersbot <biosyrok0@gmail.com>
2024-10-25 21:56:41 +0200

    g

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/All.hpp>
 
 using namespace sf;
-RenderWindow window(VideoMode(640,480), "/-(.)_(.)-\\");
-
+RenderWindow window(VideoMode(640,480), "\\/(O)_(O)\\/");
+//float grav = 0.5f;
+const float maxY = 50;
+const Vector2f gravity(0.f, 5.f);
+Vector2f velocity(2.f, 5.f);
 
 
 int main(){
@@ -12,11 +15,12 @@ int main(){
     rect.setPosition(window.getSize().x / 2, window.getSize().y / 1.25);
 
     Texture pl_t;
-    pl_t.loadFromFile("/./img/pl.png");
+    pl_t.loadFromFile("./src/img/pl.png");
 
     Sprite pl;
     pl.setTexture(pl_t);
-    //pl.setOrigin(pl_t.getSize().x / 2, pl_t.getSize().y / 2);
+    pl.setOrigin(pl_t.getSize().x / 2, pl_t.getSize().y / 2);
+    pl.setPosition(window.getSize().x / 2, window.getSize().y / 2);
 
     while(window.isOpen()){
         Event event;
@@ -27,6 +31,17 @@ int main(){
             }
         }  
         if (Keyboard::isKeyPressed(Keyboard::Escape)) {window.close(); }
+        while (!pl.getGlobalBounds().intersects(rect.getGlobalBounds())){ 
+
+            pl.move(velocity);
+            if(velocity.y < maxY){
+                velocity += gravity;
+            }
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Space)) {
+            pl.move(0, -10);
+            grav.y = 0.5;
+        }
         window.clear(Color(40, 60, 90, 1));
         window.draw(rect);
         window.draw(pl);
